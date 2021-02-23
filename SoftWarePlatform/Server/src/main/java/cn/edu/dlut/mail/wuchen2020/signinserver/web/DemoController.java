eff2c57ffabf3aea3084963a8db28fe4129f79b8
Chen Wu <201436728@qq.com>
2021-02-24 00:00:05 +0800

    Add client and server

---
+++ SoftWarePlatform/Server/src/main/java/cn/edu/dlut/mail/wuchen2020/signinserver/web/DemoController.java
@@ -0,0 +1,14 @@
+package cn.edu.dlut.mail.wuchen2020.signinserver.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
---
+++ SoftWarePlatform/Server/src/test/java/cn/edu/dlut/mail/wuchen2020/signinserver/SignInServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.dlut.mail.wuchen2020.signinserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignInServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
