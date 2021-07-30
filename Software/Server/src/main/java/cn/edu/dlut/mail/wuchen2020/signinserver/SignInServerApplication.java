276b43991d279835630b06281a3c4a7e00dc184e
Chen Wu <201436728@qq.com>
2021-07-30 20:00:07 +0800

    更新后端文档

--- Software/Server/src/main/java/cn/edu/dlut/mail/wuchen2020/signinserver/SignInServerApplication.java
+++ Software/Server/src/main/java/cn/edu/dlut/mail/wuchen2020/signinserver/SignInServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * 大学生创新创业实践项目 - 人脸识别课堂签到系统后端
  * 
- * @version 0.0.2
+ * @version 0.0.1
  * @author Luo Tian
  * @author Liu Yuan
  * @author Wu Chen
